1608898f714b1c1ffbda254208d2fafbf95d7f79
Eyad Aqrabawi <41790482+eyadaq@users.noreply.github.com>
2025-02-03 10:28:07 +0000

    feat: add new functions to libft and configure VSCode settings

---
+++ includes/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eaqrabaw <eaqrabaw@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 10:13:43 by eaqrabaw          #+#    #+#             */
+/*   Updated: 2025/02/03 10:15:56 by eaqrabaw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include "./libft/includes/libft.h"
+#include "./libft/includes/ft_printf.h"
+#include "./libft/includes/get_next_line.h"
+
+
+#endif
---
+++ srcs/main.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eaqrabaw <eaqrabaw@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 10:10:15 by eaqrabaw          #+#    #+#             */
+/*   Updated: 2025/02/03 10:11:28 by eaqrabaw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+//This file will only contain the main function
+int		main(int argc, char *argv[], char **env)
+{
+	
+}
