b39ab411e951f67cbbaae568e262b8f1dc04650c
Marwan Alrifai <marwanalrfai5@gmail.com>
2025-02-22 17:20:14 +0300

    Handle the pwd + env

--- exec/exec.c
+++ exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: malrifai <malrifai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 18:58:37 by malrifai          #+#    #+#             */
-/*   Updated: 2025/02/22 15:19:51 by malrifai         ###   ########.fr       */
+/*   Updated: 2025/02/22 15:57:53 by malrifai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,28 @@ void	execute_cmds(t_cmd *cmds, int *last_exit_status, t_env **env)
 	
 	if (!cmds || !cmds->args)
 		return ;
+	if (ft_strcmp(cmds->args[0], "pwd") == 0)
+	{
+		handle_pwd();
+		*last_exit_status = 0;
+		return ;
+	}
+	if (ft_strcmp(cmds->args[0], "env") == 0)
+    {
+        handle_env(*env);
+        *last_exit_status = 0;
+        return ;
+    }
 	if (ft_strcmp(cmds->args[0], "echo") == 0)
 	{
 		handle_echo(cmds->args);
 		*last_exit_status = 0;
 		return ;
 	}
-	// if (ft_strcmp(cmds->args[0], "export") == 0)
-	// {
-	// 	handle_export(cmds->args, env);
-	// 	*last_exit_status = 0;
-	// 	return ;
-	// }
+	if (ft_strcmp(cmds->args[0], "export") == 0)
+	{
+		handle_export(cmds->args, env);
+		*last_exit_status = 0;
+		return ;
+	}
 }
