421883107d6c17ccd736a8d716f1b5cc2a0b4da3
Marwan Alrifai <marwanalrfai5@gmail.com>
2025-02-22 15:23:54 +0300

    stop working on export, alot of cases need to be handled

--- exec/exec.c
+++ exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: malrifai <malrifai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 18:58:37 by malrifai          #+#    #+#             */
-/*   Updated: 2025/02/19 18:10:51 by malrifai         ###   ########.fr       */
+/*   Updated: 2025/02/22 15:19:51 by malrifai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	execute_cmds(t_cmd *cmds, int *last_exit_status, t_env **env)
 {
+	
 	if (!cmds || !cmds->args)
 		return ;
 	if (ft_strcmp(cmds->args[0], "echo") == 0)
@@ -22,10 +23,10 @@ void	execute_cmds(t_cmd *cmds, int *last_exit_status, t_env **env)
 		*last_exit_status = 0;
 		return ;
 	}
-	if (ft_strcmp(cmds->args[0], "export") == 0)
-	{
-		handle_export(cmds->args, env);
-		*last_exit_status = 0;
-		return ;
-	}
+	// if (ft_strcmp(cmds->args[0], "export") == 0)
+	// {
+	// 	handle_export(cmds->args, env);
+	// 	*last_exit_status = 0;
+	// 	return ;
+	// }
 }
