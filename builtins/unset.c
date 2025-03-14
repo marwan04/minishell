86b1d2fb534a33cccd125ab45c466cd8704e5655
Marwan Alrifai <marwanalrfai5@gmail.com>
2025-03-14 15:10:54 +0300

    Handle unset

---
+++ builtins/unset.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malrifai <malrifai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 14:55:50 by malrifai          #+#    #+#             */
+/*   Updated: 2025/03/14 15:08:33 by malrifai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void    unset_env_variable(t_env **env, char *key)
+{
+    t_env *current = *env;
+    t_env *prev = NULL;
+
+    while (current)
+    {
+        if (ft_strcmp(current->key, key) == 0)
+        {
+            if (prev)
+                prev->next = current->next;
+            else
+                *env = current->next;
+
+            free(current->key);
+            free(current->value);
+            free(current);
+            return;
+        }
+        prev = current;
+        current = current->next;
+    }
+}
+
+void handle_unset(char **args, t_env **env)
+{
+    int i = 1;
+
+    if (!args[i])
+        return ;
+    while (args[i])
+    {
+        if (!is_valid_identifier(args[i]))
+            printf("bash: unset: `%s': not a valid identifier\n", args[i]);
+        else
+            unset_env_variable(env, args[i]);
+        i++;
+    }
+}
