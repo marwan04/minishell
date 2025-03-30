9d9f269c3b45bc929a98f87058fc3070fb1d986e
Marwan Alrifai <marwanalrfai5@gmail.com>
2025-03-31 01:17:10 +0300

    Clean cd(protactions and leaks)

--- builtins/cd.c
+++ builtins/cd.c
@@ -3,28 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malrifai <malrifai@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alrfa3i <alrfa3i@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 22:12:06 by malrifai          #+#    #+#             */
-/*   Updated: 2025/03/17 17:47:17 by malrifai         ###   ########.fr       */
+/*   Updated: 2025/03/31 01:13:32 by alrfa3i          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	update_pwd_env(t_env **env)
+int update_pwd_env(t_env **env)
 {
 	char	*oldpwd;
 	char	*newpwd;
+	int		should_free_oldpwd;
 
+	should_free_oldpwd = 0;
 	oldpwd = get_env_value(*env, "PWD");
+	if (!oldpwd)
+	{
+		oldpwd = ft_strdup("");
+		if (!oldpwd)
+		{
+			perror("cd: malloc");
+			return (1);
+		}
+		should_free_oldpwd = 1;
+	}
 	newpwd = getcwd(NULL, 0);
-	if (!newpwd || !oldpwd)
-		exit(0);
+	if (!newpwd)
+	{
+		perror("cd: getcwd");
+		if (should_free_oldpwd)
+			free(oldpwd);
+		return (1);
+	}
 	add_or_update_env(env, "OLDPWD", oldpwd);
 	add_or_update_env(env, "PWD", newpwd);
+	if (should_free_oldpwd)
+		free(oldpwd);
 	free(newpwd);
+	return (0);
 }
+
+
 
 int	no_args(t_env **env, char **target_dir)
 {
@@ -58,10 +80,11 @@ void	handle_cd(char **args, t_env **env)
 	}
 	else
 		target_dir = args[1];
-	if (chdir(target_dir) == -1)
+		if (chdir(target_dir) == -1)
 	{
 		perror("cd");
 		return ;
 	}
-	update_pwd_env(env);
+	if (update_pwd_env(env) != 0)
+		return ;
 }
