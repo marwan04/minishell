3ceb2bab49849bca799de39d6eb9ba3bd4f5d6c9
Marwan Alrifai <marwanalrfai5@gmail.com>
2025-04-01 20:23:15 +0300

    Did multiple pipes execuation

--- exec/exec_pipes.c
+++ exec/exec_pipes.c
@@ -6,86 +6,107 @@
 /*   By: malrifai <malrifai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 08:05:54 by eaqrabaw          #+#    #+#             */
-/*   Updated: 2025/04/01 16:00:23 by malrifai         ###   ########.fr       */
+/*   Updated: 2025/04/01 20:04:54 by malrifai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	ft_exec_cmd1(t_cmd *cmd, int pipefd[2], t_env *env, int *last_exit_status)
-{
-	if (dup2(pipefd[1], STDOUT_FILENO) == -1)
-	{
-		perror("dup2");
-		exit(1);
-	}
-	close(pipefd[0]);
-	close(pipefd[1]);
-	if (is_builtin(cmd->args[0]))
-		execute_builtin_cmds(cmd, last_exit_status, &env);
-	else
-		ft_execute_command(cmd, last_exit_status, &env);
-	exit(*last_exit_status);
-}
+// void	ft_exec_cmd1(t_cmd *cmd, int pipefd[2], t_env *env, int *last_exit_status)
+// {
+// 	if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+// 	{
+// 		perror("dup2");
+// 		exit(1);
+// 	}
+// 	close(pipefd[0]);
+// 	close(pipefd[1]);
+// 	if (is_builtin(cmd->args[0]))
+// 		execute_builtin_cmds(cmd, last_exit_status, &env);
+// 	else
+// 		ft_execute_command(cmd, last_exit_status, &env);
+// 	exit(*last_exit_status);
+// }
 
-void	ft_exec_cmd2(t_cmd *cmd, int pipefd[2], t_env *env, int *last_exit_status)
-{
-	if (dup2(pipefd[0], STDIN_FILENO) == -1)
-	{
-		perror("dup2");
-		exit(1);
-	}
-	close(pipefd[0]);
-	close(pipefd[1]);
-	if (is_builtin(cmd->args[0]))
-		execute_builtin_cmds(cmd, last_exit_status, &env);
-	else
-		ft_execute_command(cmd, last_exit_status, &env);
-	exit(*last_exit_status);
-}
+// void	ft_exec_cmd2(t_cmd *cmd, int pipefd[2], t_env *env, int *last_exit_status)
+// {
+// 	if (dup2(pipefd[0], STDIN_FILENO) == -1)
+// 	{
+// 		perror("dup2");
+// 		exit(1);
+// 	}
+// 	close(pipefd[0]);
+// 	close(pipefd[1]);
+// 	if (is_builtin(cmd->args[0]))
+// 		execute_builtin_cmds(cmd, last_exit_status, &env);
+// 	else
+// 		ft_execute_command(cmd, last_exit_status, &env);
+// 	exit(*last_exit_status);
+// }
 
 void	exec_pipes(t_cmd *cmds, int *last_exit_status, t_env **env)
 {
 	int		pipefd[2];
-	pid_t	pid1;
-	pid_t	pid2;
-	int		status1;
-	int		status2;
+	int		prev_fd;
+	pid_t	pid;
+	int		status;
+	t_cmd	*current = cmds;
+	int		is_last;
+	int		child_count;
 
-	if (!cmds || !cmds->next)
-		return ;
-	if (pipe(pipefd) == -1)
+	prev_fd = -1;
+	child_count = 0;
+	while (current)
 	{
-		perror("pipe");
-		*last_exit_status = 1;
-		return;
+		if (current->next == NULL)
+			is_last = 1;
+		else
+			is_last = 0;
+		if (!is_last && pipe(pipefd) == -1)
+		{
+			perror("pipe");
+			*last_exit_status = 1;
+			return;
+		}
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			*last_exit_status = 1;
+			return;
+		}
+		if (pid == 0)
+		{
+			if (prev_fd != -1)
+			{
+				if (dup2(prev_fd, STDIN_FILENO) == -1)
+					exit(1);
+				close(prev_fd);
+			}
+			if (!is_last)
+			{
+				if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+					exit(1);
+				close(pipefd[0]);
+				close(pipefd[1]);
+			}
+			if (is_builtin(current->args[0]))
+				execute_builtin_cmds(current, last_exit_status, env);
+			else
+				ft_execute_command(current, last_exit_status, env);
+			exit(*last_exit_status);
+		}
+		if (prev_fd != -1)
+			close(prev_fd);
+		if (!is_last)
+		{
+			close(pipefd[1]);
+			prev_fd = pipefd[0];
+		}
+		current = current->next;
+		child_count++;
 	}
-	pid1 = fork();
-	if (pid1 == -1)
-	{
-		perror("fork");
-		close(pipefd[0]);
-		close(pipefd[1]);
-		*last_exit_status = 1;
-		return;
-	}
-	if (pid1 == 0)
-		ft_exec_cmd1(cmds, pipefd, *env, last_exit_status);
-	pid2 = fork();
-	if (pid2 == -1)
-	{
-		perror("fork");
-		close(pipefd[0]);
-		close(pipefd[1]);
-		waitpid(pid1, NULL, 0); // avoid zombie from first child
-		*last_exit_status = 1;
-		return;
-	}
-	if (pid2 == 0)
-		ft_exec_cmd2(cmds->next, pipefd, *env, last_exit_status);
-	close(pipefd[0]);
-	close(pipefd[1]);
-	waitpid(pid1, &status1, 0);
-	waitpid(pid2, &status2, 0);
-	ft_set_exit_status(last_exit_status, status2); // follow bash: use last command
+	while (child_count-- > 0)
+		wait(&status);
+	ft_set_exit_status(last_exit_status, status);
 }
