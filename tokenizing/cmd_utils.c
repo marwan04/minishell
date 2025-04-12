be0dcc8fcdb63ad9f6efe3285a864801ffc786da
Marwan Alrifai <marwanalrfai5@gmail.com>
2025-04-12 16:58:47 +0300

    Norm cmd_utils.c

--- tokenizing/cmd_utils.c
+++ tokenizing/cmd_utils.c
@@ -6,15 +6,15 @@
 /*   By: malrifai <malrifai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 14:07:09 by malrifai          #+#    #+#             */
-/*   Updated: 2025/04/10 15:58:01 by malrifai         ###   ########.fr       */
+/*   Updated: 2025/04/12 16:58:21 by malrifai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-t_ast *new_ast_cmd(void)
+t_ast	*new_ast_cmd(void)
 {
-	t_ast *node;
+	t_ast	*node;
 
 	node = malloc(sizeof(t_ast));
 	if (!node)
@@ -27,12 +27,14 @@ t_ast *new_ast_cmd(void)
 	return (node);
 }
 
-void add_argument(t_ast *node, char *arg)
+void	add_argument(t_ast *node, char *arg)
 {
-	int		count = 0;
-	int		i = 0;
+	int		count;
+	int		i;
 	char	**new_args;
 
+	count = 0;
+	i = 0;
 	if (!node || node->type != NODE_CMD)
 		return ;
 	while (node->args && node->args[count])
@@ -51,17 +53,15 @@ void add_argument(t_ast *node, char *arg)
 	node->args = new_args;
 }
 
-t_ast *handle_redirection(t_ast *cmd_node, t_token *token)
+t_ast	*handle_redirection(t_ast *cmd_node, t_token *token)
 {
-	t_ast *redir_node;
+	t_ast	*redir_node;
 
 	if (!cmd_node || !token || !token->next)
-		return NULL;
-
+		return (NULL);
 	redir_node = malloc(sizeof(t_ast));
 	if (!redir_node)
-		return NULL;
-
+		return (NULL);
 	if (token->type == REDIR_IN)
 		redir_node->type = NODE_REDIR_IN;
 	else if (token->type == REDIR_OUT)
@@ -70,11 +70,9 @@ t_ast *handle_redirection(t_ast *cmd_node, t_token *token)
 		redir_node->type = NODE_APPEND;
 	else if (token->type == HEREDOC)
 		redir_node->type = NODE_HEREDOC;
-
-	redir_node->file = ft_strdup(token->next->value); // The filename or limiter
+	redir_node->file = ft_strdup(token->next->value);
 	redir_node->left = cmd_node;
 	redir_node->right = NULL;
 	redir_node->args = NULL;
-
-	return redir_node;
+	return (redir_node);
 }
