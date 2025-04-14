a7e3af6f7d8f30df31cb84c79d9edc84ec575be7
Eyad Aqrabawi <41790482+eyadaq@users.noreply.github.com>
2025-04-14 07:28:57 +0000

    Update comment timestamp and clean up whitespace in cd.c

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: eaqrabaw <eaqrabaw@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 22:12:06 by malrifai          #+#    #+#             */
-/*   Updated: 2025/04/13 19:57:40 by eaqrabaw         ###   ########.fr       */
+/*   Updated: 2025/04/14 07:28:43 by eaqrabaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,6 @@ int update_pwd_env(t_env **env)
 	return (0);
 }
 
-
-
 int	no_args(t_env **env, char **target_dir)
 {
 	*target_dir = get_env_value(*env, "HOME");
@@ -64,8 +62,10 @@ void	handle_cd(char **args, t_env **env)
 	char	*target_dir;
 
 	if (!args[1])
+	{
 		if (no_args(env, &target_dir) == 1)
 			return ;
+	}
 	else if (ft_strcmp(args[1], "-") == 0)
 	{
 		target_dir = get_env_value(*env, "OLDPWD");
